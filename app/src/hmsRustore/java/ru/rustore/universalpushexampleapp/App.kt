79d7adbd9fd5b1774ba9e2429678b4d2cc3155ba
igor.bobrov <igor.bobrov@vk.team>
2023-05-03 11:34:26 +0300

    biuild: добавил 3 flavor: fcmRustoreHms, fcmRustore, hmsRustore; зависимости подтягиваются для нужных flavor; обновил версии зависимостей
    refactor: обновил экран приложения, добавил возможность копировать токен

---
+++ app/src/hmsRustore/java/ru/rustore/universalpushexampleapp/App.kt
@@ -0,0 +1,55 @@
+package ru.rustore.universalpushexampleapp
+
+import android.app.Application
+import android.util.Log
+import ru.rustore.sdk.core.tasks.OnCompleteListener
+import ru.rustore.sdk.universalpush.RuStoreUniversalPushClient
+import ru.rustore.sdk.universalpush.firebase.provides.FirebasePushProvider
+import ru.rustore.sdk.universalpush.hms.providers.HmsPushProvider
+import ru.rustore.sdk.universalpush.rustore.logger.DefaultLogger
+import ru.rustore.sdk.universalpush.rustore.providers.RuStorePushProvider
+
+class App: Application() {
+
+    private val tag = "UniversalPushExampleApp"
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // ru.rustore.universalpushexampleapp
+        RuStoreUniversalPushClient.init(
+            context = this,
+            rustore = RuStorePushProvider(
+                application = this,
+                projectId = "m3Id6aPeXq36mpNb5q000IodEDHlGhL0",
+                logger = DefaultLogger(tag = tag),
+            ),
+            hms = HmsPushProvider(
+                context = this,
+                appid = "108003365",
+            ),
+        )
+
+        RuStoreUniversalPushClient.getTokens()
+            .addOnCompleteListener(object : OnCompleteListener<Map<String, String>> {
+                override fun onSuccess(result: Map<String, String>) {
+                    Log.w(tag, "get tokens success ${result}")
+                }
+
+                override fun onFailure(throwable: Throwable) {
+                    Log.e(tag, "get tokens err: ${throwable}")
+                }
+            })
+
+        RuStoreUniversalPushClient.checkAvailability(this)
+            .addOnCompleteListener(object : OnCompleteListener<Map<String, Boolean>> {
+                override fun onSuccess(result: Map<String, Boolean>) {
+                    Log.w(tag, "get availability success ${result}")
+                }
+
+                override fun onFailure(throwable: Throwable) {
+                    Log.e(tag, "get tokens err: ${throwable}")
+                }
+            })
+    }
+}
--- app/src/main/java/ru/rustore/universalpushexampleapp/MainActivity.kt
+++ app/src/main/java/ru/rustore/universalpushexampleapp/MainActivity.kt
@@ -1,25 +1,39 @@
 package ru.rustore.universalpushexampleapp
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.rustore.sdk.core.tasks.OnCompleteListener
 import ru.rustore.sdk.universalpush.RuStoreUniversalPushClient
 import ru.rustore.sdk.universalpush.UNIVERSAL_FCM_PROVIDER
 import ru.rustore.sdk.universalpush.UNIVERSAL_HMS_PROVIDER
 import ru.rustore.sdk.universalpush.UNIVERSAL_RUSTORE_PROVIDER
+import ru.rustore.universalpushexampleapp.theme.PushTheme
+
 
 class MainActivity : ComponentActivity() {
-    val viewModel = UniversalPushViewModel()
+    val viewModel by viewModels<UniversalPushViewModel> ()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,34 +65,119 @@ class MainActivity : ComponentActivity() {
             })
 
         setContent {
-            Screen(viewModel = viewModel)
+            PushTheme {
+                Screen(viewModel = viewModel)
+            }
         }
     }
 }
 
+@Preview
+@Composable
+fun SimpleComposablePreview() {
+    val viewModel = UniversalPushViewModel()
+    PushTheme {
+        Screen(viewModel = viewModel)
+    }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Screen(viewModel: UniversalPushViewModel) {
-    Surface(
+    Scaffold(
         modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column {
-            Text("Availability Universal Push SDK")
-            Text("hms availability: ${viewModel.hmsAvailability.value}")
-            Text("fcm availability: ${viewModel.fcmAvailability.value}")
-            Text("rustore availability: ${viewModel.rustoreAvailability.value}")
-            Divider(
-                color = Color.Black,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 4.dp)
-                    .height(1.dp)
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        color = MaterialTheme.colorScheme.surface
+                    )
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = MaterialTheme.colorScheme.primary)
             )
-            Text("Tokens Universal Push SDK")
-            Text("hms token: ${viewModel.hmsToken.value}")
-            Text("fcm token: ${viewModel.fcmToken.value}")
-            Text("rustore token: ${viewModel.rustoreToken.value}")
+        }) {
+        Column(modifier = Modifier.padding(it)) {
+            TokenCard(
+                type = "HMS",
+                availability = viewModel.hmsAvailability,
+                token = viewModel.hmsToken
+            )
+            TokenCard(
+                type = "FCM",
+                availability = viewModel.fcmAvailability,
+                token = viewModel.fcmToken
+            )
+            TokenCard(
+                type = "RuStore",
+                availability = viewModel.rustoreAvailability,
+                token = viewModel.rustoreToken
+            )
         }
     }
+}
+
+@Composable
+fun TokenCard(
+    type: String,
+    availability: MutableState<Boolean>,
+    token: MutableState<String>
+) {
+    val context = LocalContext.current
+
+    Card(
+        border = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.secondary),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(2.dp)
+    ) {
+        Text(
+            modifier = Modifier.padding(4.dp),
+            text = buildAnnotatedString {
+                withStyle(SpanStyle(fontWeight = FontWeight.Bold)){
+                    append("$type availability: ")
+                }
+                append("${availability.value}")
+            }
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                modifier = Modifier
+                    .weight(1f, fill = false)
+                    .padding(4.dp),
+                text = buildAnnotatedString {
+                    withStyle(SpanStyle(fontWeight = FontWeight.Bold)){
+                        append("Token: ")
+                    }
+                    append(token.value.ifEmpty { "Unavailable" })
+                }
+            )
+            IconButton(
+                onClick = {
+                    copyToClipBoard(context = context, text = token.value)
+                    Toast.makeText(context, "Token is copied to clipboard", Toast.LENGTH_LONG)
+                        .show()
+                },
+                enabled = token.value.isNotEmpty()
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_content_copy_24),
+                    contentDescription = "copy"
+                )
+            }
+        }
+    }
+}
+
+fun copyToClipBoard(context: Context, text: String) {
+    val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    val clip = ClipData.newPlainText("token", text)
+    clipboard.setPrimaryClip(clip)
 }
